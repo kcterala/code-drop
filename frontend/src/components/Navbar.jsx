2da79908b489aacffd658132ed0f877d82e2bf37
kcterala <kcterala@gmail.com>
2024-04-10 12:06:46 +0530

    Add Github link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='p-10 flex justify-between'>
-        <h1 className='text-white text-3xl font-bold ml-8 hover:text-green-500'>Slim IDE</h1>
+        <h1 className='text-white text-3xl font-bold ml-8 hover:text-green-500'>CodeDrop</h1>
         <div className='flex justify-between gap-5'>
-            <h1 className='text-2xl text-white'>Github</h1>
+            <h1 className='text-2xl text-white'><a href='https://github.com/kcterala/code-judge'>Github</a></h1>
         </div>
     </div>
   )
